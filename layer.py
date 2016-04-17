5cd27cf264941f6449d275a2689ee9bb73e5f09b
jmmartinez <jm.martinez84@live.com>
2016-04-17 17:04:45 +0200

    copy echo layer

--- layer.py
+++ layer.py
@@ -12,21 +12,32 @@ class HomeLayer(YowInterfaceLayer):
 
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
-        #send receipt otherwise we keep receiving the same message over and over
 
-        if True:
-            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
+        if messageProtocolEntity.getType() == 'text':
+            self.onTextMessage(messageProtocolEntity)
+        elif messageProtocolEntity.getType() == 'media':
+            self.onMediaMessage(messageProtocolEntity)
 
-            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
-                messageProtocolEntity.getBody(),
-                to = messageProtocolEntity.getFrom())
+        self.toLower(messageProtocolEntity.forward(messageProtocolEntity.getFrom()))
+        self.toLower(messageProtocolEntity.ack())
+        self.toLower(messageProtocolEntity.ack(True))
 
-            self.toLower(receipt)
-            self.toLower(outgoingMessageProtocolEntity)
 
     @ProtocolEntityCallback("receipt")
     def onReceipt(self, entity):
-        print("ACK:")
-        print(entity)
-        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
-        self.toLower(ack)
+        self.toLower(entity.ack())
+
+    def onTextMessage(self,messageProtocolEntity):
+        # just print info
+        print("Echoing %s to %s" % (messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(False)))
+
+    def onMediaMessage(self, messageProtocolEntity):
+        # just print info
+        if messageProtocolEntity.getMediaType() == "image":
+            print("Echoing image %s to %s" % (messageProtocolEntity.url, messageProtocolEntity.getFrom(False)))
+
+        elif messageProtocolEntity.getMediaType() == "location":
+            print("Echoing location (%s, %s) to %s" % (messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude(), messageProtocolEntity.getFrom(False)))
+
+        elif messageProtocolEntity.getMediaType() == "vcard":
+            print("Echoing vcard (%s, %s) to %s" % (messageProtocolEntity.getName(), messageProtocolEntity.getCardData(), messageProtocolEntity.getFrom(False)))
