4df2676b15dd86f850e101cb04498406607346d1
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-18 10:35:55 +0200

    import drc

--- layer.py
+++ layer.py
@@ -3,6 +3,7 @@
 
 import threading, time
 from settings import settings
+from DjangoRestClient import DjangoRestClient
 from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
 from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
 from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
