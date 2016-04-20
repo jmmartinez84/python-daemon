b83e7355949a7d30c7f187d4c9c328703e1ed2a4
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-20 15:54:06 +0200

    added exit handler test

---
+++ test_exit_handler.py
@@ -0,0 +1,21 @@
+import os, sys
+def set_exit_handler(func):
+    if os.name == "nt":
+        try:
+            import win32api
+            win32api.SetConsoleCtrlHandler(func, True)
+        except ImportError:
+            version = “.”.join(map(str, sys.version_info[:2]))
+            raise Exception(”pywin32 not installed for Python ” + version)
+    else:
+        import signal
+        signal.signal(signal.SIGTERM, func)
+if __name__ == "__main__":
+    def on_exit(sig, func=None):
+        print "exit handler triggered"
+        import time
+        time.sleep(5)
+    set_exit_handler(on_exit)
+    print "Press  to quit"
+    raw_input()
+    print "quit!"
