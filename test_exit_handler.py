eb7ac1b0c7177613f95f56e5aacb06048518b022
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-20 15:56:02 +0200

    fixed encoding issue

--- test_exit_handler.py
+++ test_exit_handler.py
@@ -1,15 +1,7 @@
 import os, sys
+import signal
 def set_exit_handler(func):
-    if os.name == "nt":
-        try:
-            import win32api
-            win32api.SetConsoleCtrlHandler(func, True)
-        except ImportError:
-            version = “.”.join(map(str, sys.version_info[:2]))
-            raise Exception(”pywin32 not installed for Python ” + version)
-    else:
-        import signal
-        signal.signal(signal.SIGTERM, func)
+	signal.signal(signal.SIGTERM, func)
 if __name__ == "__main__":
     def on_exit(sig, func=None):
         print "exit handler triggered"
