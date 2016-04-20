b0ce1b5fe61bf20de37854171b3b605216b4c91a
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-20 15:59:45 +0200

    debuggin

--- test_exit_handler.py
+++ test_exit_handler.py
@@ -1,11 +1,10 @@
-import os, sys
-import signal
+import signal, time
 def set_exit_handler(func):
+	print "set_exit_handler"
 	signal.signal(signal.SIGTERM, func)
 if __name__ == "__main__":
     def on_exit(sig, func=None):
         print "exit handler triggered"
-        import time
         time.sleep(5)
     set_exit_handler(on_exit)
     print "Press  to quit"
