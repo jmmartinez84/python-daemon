10b479c3a4a89b57841bcdd99c543bd7015fc6d4
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-27 10:01:06 +0200

    added start and test handeler

--- homebot.py
+++ homebot.py
@@ -1,13 +1,19 @@
 import telegram
 from telegram.ext import Updater
 from settings import settings
+from telegram.ext import CommandHandler
 token = settings.get('Homebot')['token']
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
 updater.start_polling()
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 def test(bot, update):
     custom_keyboard = [[ telegram.KeyboardButton(telegram.Emoji.THUMBS_UP_SIGN),telegram.KeyboardButton(telegram.Emoji.THUMBS_DOWN_SIGN) ]]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     results = bot.sendMessage(chat_id=update.message.chat_id, text="Test", reply_markup=reply_markup)
     print results
-dispatcher.addTelegramCommandHandler('test', test)
+start_handler = CommandHandler('start', start)
+dispatcher.addHandler(start_handler)
+test_handler = CommandHandler('test', test)
+dispatcher.addHandler(test_handler)
