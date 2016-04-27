a368b97dffd4d312ae18ee18ad7a6d72911e0d45
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-27 12:36:20 +0200

    added example job

--- homebot.py
+++ homebot.py
@@ -2,9 +2,12 @@ import telegram
 from telegram.ext import Updater
 from settings import settings
 from telegram.ext import CommandHandler
-token = settings.get('Homebot')['token']
+my_settings = settings.get('Homebot')
+token = my_settings['token']
+chat_id = my_settings['chat_id'];
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
+jobs. = updater.job_queue
 updater.start_polling()
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
@@ -13,7 +16,10 @@ def test(bot, update):
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     results = bot.sendMessage(chat_id=update.message.chat_id, text="Test", reply_markup=reply_markup)
     print results
+def job2(bot):
+    bot.sendMessage(chat_id=chat_id, text='A single message with 30s delay')
 start_handler = CommandHandler('start', start)
 dispatcher.addHandler(start_handler)
 test_handler = CommandHandler('test', test)
 dispatcher.addHandler(test_handler)
+jobs.put(job2, 30, repeat=False)
