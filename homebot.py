209d7cd663999f71663dad80566a671144eae480
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-27 15:50:46 +0200

    change command

--- homebot.py
+++ homebot.py
@@ -11,16 +11,16 @@ jobs = updater.job_queue
 updater.start_polling()
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-def test(bot, update):
-    custom_keyboard = [[ telegram.KeyboardButton(telegram.Emoji.THUMBS_UP_SIGN),telegram.KeyboardButton(telegram.Emoji.THUMBS_DOWN_SIGN) ]]
+def wifi(bot, update):
+    custom_keyboard = [[ telegram.KeyboardButton("On"),telegram.KeyboardButton("Off"), telegram.KeyboardButton("Cancel") ]]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    results = bot.sendMessage(chat_id=update.message.chat_id, text="Test", reply_markup=reply_markup)
+    results = bot.sendMessage(chat_id=update.message.chat_id, text="Wifi:", reply_markup=reply_markup)
     print results
 def job2(bot):
     reply_markup = telegram.ReplyKeyboardHide()
     bot.sendMessage(chat_id=chat_id, text='A single message with 30s delay',reply_markup=reply_markup)
 start_handler = CommandHandler('start', start)
 dispatcher.addHandler(start_handler)
-test_handler = CommandHandler('test', test)
-dispatcher.addHandler(test_handler)
+wifi_handler = CommandHandler('wifi', wifi)
+dispatcher.addHandler(wifi_handler)
 jobs.put(job2, 30, repeat=False)
