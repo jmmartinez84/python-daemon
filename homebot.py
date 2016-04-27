a4438d4745036915028608f0b3ccf4af31be69e4
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-27 09:57:02 +0200

    changed keyboard

--- homebot.py
+++ homebot.py
@@ -6,6 +6,8 @@ updater = Updater(token=token)
 dispatcher = updater.dispatcher
 updater.start_polling()
 def test(bot, update):
-    results = bot.sendMessage(chat_id=update.message.chat_id, text="Test", reply_markup={"keyboard":[["Test1"], ["Test2"], ["Test3"], ["Test4"]})
+    custom_keyboard = [[ telegram.KeyboardButton(telegram.Emoji.THUMBS_UP_SIGN),telegram.KeyboardButton(telegram.Emoji.THUMBS_DOWN_SIGN) ]]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    results = bot.sendMessage(chat_id=update.message.chat_id, text="Test", reply_markup=reply_markup)
     print results
 dispatcher.addTelegramCommandHandler('test', test)
