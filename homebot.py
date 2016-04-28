6fb46eec9b096e70773a7e264f9d7eff40b88f51
Jose Manuel Martínez <jmmartienz@me.com>
2016-04-28 14:35:54 +0200

    added text_message_handler

--- homebot.py
+++ homebot.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import telegram
-from telegram.ext import Updater
+from telegram.ext import Updater, MessageHandler, Filters
 from settings import settings
 from telegram.ext import CommandHandler
 from DjangoRestClient import DjangoRestClient
@@ -10,17 +10,23 @@ sys.path.insert(0, '/home/pi/router')
 from RouterAPI import RouterAPI
 
 import logging
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
                     
 credentials = settings.get('DjangoREST')
 my_settings = settings.get('Homebot')
 token = my_settings['token']
-chat_id = my_settings['chat_id'];
+job_chat_id = my_settings['chat_id'];
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
 jobs = updater.job_queue
 updater.start_polling()
+updater.idle()
+def text_message_handler(bot, update):
+    chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
+    text = update.message.text
+    print user_id
 def set_wifi(value):
     r_api = RouterAPI();
     r_api.log_in();
@@ -29,6 +35,8 @@ def set_wifi(value):
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 def wifi(bot, update):
+    chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
     custom_keyboard = [[ telegram.KeyboardButton("On"),telegram.KeyboardButton("Off"), telegram.KeyboardButton("Cancel") ]]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard,  resize_keyboard = False, one_time_keyboard=True)
     results = bot.sendMessage(chat_id=update.message.chat_id, text="Wifi:", reply_markup=reply_markup)
@@ -37,7 +45,7 @@ def job_alerts(bot):
     drc = DjangoRestClient(credentials['url'], credentials['user'], credentials['pwd'])
     alerts = drc.get_alerts_not_sent()
     for alert in alerts:
-        bot.sendMessage(chat_id=chat_id, text=alert['alert_text'])
+        bot.sendMessage(chat_id=job_chat_id, text=alert['alert_text'])
         drc.set_alert_as_sent(alert)
 start_handler = CommandHandler('start', start)
 dispatcher.addHandler(start_handler)
